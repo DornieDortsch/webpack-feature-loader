e654bdf3121899ca8797820b79dbdc0b1a558519
DornieDortsch <dornie.dortsch@gmail.com>
2018-04-15 22:27:51 +0200

    add buttons to execute features

--- src/feature-loader.js
+++ src/feature-loader.js
@@ -1,10 +1,32 @@
+const createFeatureComponent = feature => {
+    var element = document.createElement('a')
+    
+    element.href= '#'
+    element.style.display = 'block'
+    element.innerHTML = 'execute ' + feature
+    element.onclick = () => {
+        executeFeature(feature)
+        return false
+    }
+    
+    return element
+}
+
+const executeFeature = name => {
+    loadFeature(name).then(feature => {
+        console.log('feature ' + name + ' loaded')
+        feature.default()
+    })
+}
+
 const loadFeature = name => {
-    import(/* webpackChunkName: "feature/[request]" */ '/' + name + '.js')
-        .then(feature => feature.default())
+    return import(/* webpackChunkName: "feature/[request]" */ '/' + name + '.js')
 }
 
 console.log('start feature-loader')
 
-loadFeature('feature')
+executeFeature('feature')
+executeFeature('subfeature')
 
-loadFeature('subfeature')
+document.body.appendChild(createFeatureComponent('feature'))
+document.body.appendChild(createFeatureComponent('subfeature'))
