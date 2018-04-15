9833dcc2b3fc2f20b157092f104cd3f4bc281e09
DornieDortsch <dornie.dortsch@gmail.com>
2018-04-15 22:29:05 +0200

    update styles

--- src/feature-loader.js
+++ src/feature-loader.js
@@ -1,8 +1,9 @@
 const createFeatureComponent = feature => {
-    var element = document.createElement('a')
+    var element = document.createElement('button')
     
     element.href= '#'
-    element.style.display = 'block'
+    element.style.display = 'inline-block'
+    element.style.margin = '8px 0 8px 8px'
     element.innerHTML = 'execute ' + feature
     element.onclick = () => {
         executeFeature(feature)
